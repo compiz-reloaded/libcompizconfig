2b9fe290eccc425544f7dd4063b577bce814747a
onestone <onestone>
2007-03-13 20:39:15 +0000

    libbs: completed settings loading

--- src/compiz.c
+++ src/compiz.c
@@ -12,9 +12,164 @@
 
 #include "bsettings-private.h"
 
+static void initValue(BSSettingValue * value, CompOptionValue * from,
+					  BSSettingType type)
+{
+	switch(type)
+	{
+		case TypeInt:
+			value->value.asInt = from->i;
+			break;
+		case TypeFloat:
+			value->value.asFloat = from->f;
+			break;
+		case TypeBool:
+			value->value.asBool = from->b;
+			break;
+		case TypeColor:
+			{
+				int i;
+				for (i=0;i<4;i++)
+					value->value.asColor.array.array[i] = from->c[i];
+			}
+			break;
+		case TypeString:
+			value->value.asString = strdup(from->s);
+			break;
+		case TypeMatch:
+			value->value.asMatch = matchToString(&from->match);
+			break;
+		case TypeAction:
+			value->value.asAction.button = from->action.button.button;
+			value->value.asAction.buttonModMask = from->action.button.modifiers;
+			value->value.asAction.keysym = from->action.key.keysym;
+			value->value.asAction.keyModMask = from->action.key.modifiers;
+			value->value.asAction.onBell = from->action.bell;
+			value->value.asAction.edgeMask = from->action.edgeMask;
+			break;
+		default:
+			break;
+	}
+}
+
+static void initInfo(BSSettingInfo * info, CompOption * o, BSSettingType type)
+{
+	switch(type)
+	{
+		case TypeInt:
+			info->forInt.min = o->rest.i.min;
+			info->forInt.max = o->rest.i.max;
+			break;
+		case TypeFloat:
+			info->forFloat.min = o->rest.f.min;
+			info->forFloat.max = o->rest.f.max;
+			info->forFloat.precision = o->rest.f.precision;
+			break;
+		case TypeString:
+			if (o->rest.s.nString && o->rest.s.string)
+			{
+				int i;
+				for (i = 0; i < o->rest.s.nString; i++)
+				{
+					info->forString.allowedValues =
+							bsStringListAppend(info->forString.allowedValues,
+											   strdup(o->rest.s.string[i]));
+				}
+			}
+			break;
+		case TypeAction:
+			info->forAction.key =
+					(o->value.action.state & CompActionStateInitKey) ||
+					(o->value.action.state & CompActionStateTermKey);
+			info->forAction.button =
+					(o->value.action.state & CompActionStateInitButton) ||
+					(o->value.action.state & CompActionStateTermButton);
+			info->forAction.edge =
+					(o->value.action.state & CompActionStateInitEdge) ||
+					(o->value.action.state & CompActionStateTermEdge);
+			info->forAction.bell =
+					(o->value.action.state & CompActionStateInitBell) && TRUE;
+			break;
+		default:
+			break;
+	}
+}
+
+static BSSettingType translateType(CompOptionType type)
+{
+	switch(type)
+	{
+		case CompOptionTypeInt:
+			return TypeInt;
+		case CompOptionTypeFloat:
+			return TypeFloat;
+		case CompOptionTypeString:
+			return TypeString;
+		case CompOptionTypeColor:
+			return TypeColor;
+		case CompOptionTypeBool:
+			return TypeBool;
+		case CompOptionTypeAction:
+			return TypeAction;
+		case CompOptionTypeList:
+			return TypeList;
+		case CompOptionTypeMatch:
+			return TypeMatch;
+		default:
+			break;
+	}
+	return TypeNum;
+}
+
 static void initOption(BSPlugin * plugin, CompOption * o, Bool isScreen)
 {
 	printf("Reading Option %s\n", o->name);
+	NEW(BSSetting, setting);
+	
+	setting->parent     = plugin;
+	setting->isScreen   = isScreen;
+	setting->screenNum  = 0;
+	setting->isDefault  = TRUE;
+	setting->name       = strdup(o->name);
+	setting->shortDesc  = strdup(o->shortDesc);
+	setting->longDesc   = strdup(o->longDesc);
+	setting->group      = strdup("");
+	setting->subGroup   = strdup("");
+	setting->hints      = strdup("");
+	setting->type       = translateType(o->type);
+	
+	setting->defaultValue.parent = setting;
+	
+	if (setting->type == TypeList)
+	{
+		NEW(BSSettingInfo, listInfo);
+		setting->info.forList.listType = translateType(o->value.list.type);
+		setting->info.forList.listInfo = listInfo;
+		
+		initInfo(setting->info.forList.listInfo, o,
+				 setting->info.forList.listType);
+		int i;
+		for (i = 0; i < o->value.list.nValue; i++)
+		{
+			NEW(BSSettingValue, val);
+			val->parent = setting;
+			val->isListChild = TRUE;
+			initValue(val, &o->value.list.value[i],
+					  setting->info.forList.listType);
+			setting->defaultValue.value.asList = bsSettingValueListAppend(
+					setting->defaultValue.value.asList, val);
+		}
+	}
+	else
+	{
+		initInfo(&setting->info, o, setting->type);
+		initValue(&setting->defaultValue, &o->value, setting->type);
+	}
+
+	setting->value = &setting->defaultValue;
+	
+	plugin->settings = bsSettingListAppend(plugin->settings, setting);
+	
 }
 
 void bsLoadPlugin(BSContext * context, char * filename)
@@ -51,14 +206,14 @@ void bsLoadPlugin(BSContext * context, char * filename)
 		dlclose (dlhand);
 		return;
 	}
-	/*
+	
 	//make sure a plugin by the same name isn't already loaded
-	if (beryl_settings_context_find_plugin(context,vt->name))
+	if (bsFindPlugin(context, vt->name))
 	{
 		dlclose(dlhand);
 		return;
 	}
-	*/
+	
 	NEW(BSPlugin,plugin);
 	
 	plugin->context = context;
@@ -117,30 +272,32 @@ void bsLoadPlugin(BSContext * context, char * filename)
 		}
 	}
 
-	/*
-	if (!beryl_settings_plugin_find_setting(plugin,"____plugin_enabled",FALSE))
+	if (!bsFindSetting(plugin, "____plugin_enabled", FALSE, 0))
 	{
-		NEW(BerylSetting,setting);
-		setting->parent=plugin;
-		setting->is_screen=FALSE;
-		setting->is_default=TRUE;
-		setting->name=g_strdup("____plugin_enabled");
-		setting->short_desc=g_strdup("enabled");
-		setting->long_desc=g_strdup("enabled");
-		setting->group=g_strdup("");
-		setting->subGroup=g_strdup("");
-		setting->type=BERYL_SETTING_TYPE_BOOL;
-		setting->advanced=TRUE;
-		setting->default_value.parent=setting;
-		setting->default_value.value.as_bool=vt->defaultEnabled;
-		copy_from_default(setting);
-		plugin->settings=g_slist_append(plugin->settings,setting);
+		NEW(BSSetting, setting);
+		
+		setting->parent                    = plugin;
+		setting->isScreen                  = FALSE;
+		setting->screenNum                 = 0;
+		setting->isDefault                 = TRUE;
+		setting->name                      = strdup("____plugin_enabled");
+		setting->shortDesc                 = strdup("enabled");
+		setting->longDesc                  = strdup("enabled");
+		setting->group                     = strdup("");
+		setting->subGroup                  = strdup("");
+		setting->type                      = TypeBool;
+		setting->defaultValue.parent       = setting;
+		setting->defaultValue.value.asBool = FALSE;
+		
+		setting->value = &setting->defaultValue;
+	
+		plugin->settings = bsSettingListAppend(plugin->settings, setting);
 	}
 	else
 	{
-		g_error("Couldn't load plugin %s because it has a setting defined named ____plugin_enabled",plugin->name);
+		fprintf(stderr, "Couldn't load plugin %s because it has a setting "
+						"defined named ____plugin_enabled", plugin->name);
 	}
-	*/
 	
 	dlclose(dlhand);
 	//collate_groups(plugin);
