e19791a1a7c2be49486472933d451574037eca50
onestone <onestone>
2007-03-14 14:52:53 +0000

    libbs: more string list setter/getter functions

--- src/lists.c
+++ src/lists.c
@@ -5,6 +5,8 @@
 
 #include <bsettings.h>
 
+#include "bsettings-private.h"
+
 typedef void (*freeFunc) (void *ptr);
 
 #define BSLIST(type,dtype) \
@@ -116,3 +118,51 @@ BSLIST(Group,BSGroup)
 BSLIST(SubGroup,BSSubGroup)
 BSLIST(SettingValue,BSSettingValue)
 
+BSSettingValueList * bsGetValueListFromStringList(BSStringList *list)
+{
+	BSSettingValueList *rv = NULL;
+	while (list)
+	{
+		NEW(BSSettingValue, value);
+		value->isListChild = TRUE;
+		value->value.asString = strdup(list->data);
+		rv = bsSettingValueListAppend(rv, value);
+		list = list->next;
+	}
+	return rv;
+}
+
+BSStringList * bsGetStringListFromValueList(BSSettingValueList *list)
+{
+	BSStringList *rv = NULL;
+	while (list)
+	{
+		rv = bsStringListAppend(rv, strdup(list->data->value.asString));
+		list = list->next;
+	}
+	return rv;
+}
+
+char ** bsGetStringArrayFromList(BSStringList *list, int *num)
+{
+	char ** rv = NULL;
+	int length = bsStringListLength(list);
+	int i;
+	
+	if (length)
+		rv = malloc(length * sizeof(char *));
+
+	for (i = 0; i < length; i++, list = list->next)
+		rv[i] = strdup(list->data);
+	*num = length;
+	return rv;
+}
+
+BSStringList * bsGetListFromStringArray(char ** array, int num)
+{
+	BSStringList *rv = NULL;
+	int i;
+	for (i = 0; i < num; i++)
+		rv = bsStringListAppend(rv, strdup(array[i]));
+	return rv;
+}
