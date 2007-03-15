d17ca60310f0c2dd17ca07300f4e361e6fe95c08
onestone <onestone>
2007-03-15 14:05:04 +0000

    libbs: added integrated configuration system

--- src/bsettings-private.h
+++ src/bsettings-private.h
@@ -9,4 +9,13 @@
 void bsLoadPlugin(BSContext * context, char * filename);
 void collateGroups(BSPlugin * plugin);
 
+typedef enum {
+	OptionProfile,
+	OptionBackend,
+	OptionIntegration
+} ConfigOption;
+
+Bool bsReadConfig(ConfigOption option, char** value);
+Bool bsWriteConfig(ConfigOption option, char* value);
+
 #endif
