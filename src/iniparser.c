015656e20395f27e94291075ec15473d1eef0765
Danny Baumann <dannybaumann@web.de>
2007-04-19 15:12:58 +0200

    Fixed ini file formatting.

--- src/iniparser.c
+++ src/iniparser.c
@@ -540,20 +540,20 @@ void iniparser_dump_ini(dictionary * d, FILE * f)
     for (i=0 ; i<nsec ; i++) {
         secname = iniparser_getsecname(d, i) ;
         seclen  = (int)strlen(secname);
-        fprintf(f, "\n[%s]\n", secname);
+        fprintf(f, "[%s]\n", secname);
         sprintf(keym, "%s:", secname);
         for (j=0 ; j<d->size ; j++) {
             if (d->key[j]==NULL)
                 continue ;
             if (!strncmp(d->key[j], keym, seclen+1)) {
                 fprintf(f,
-                        "%-30s = %s\n",
+                        "%s = %s\n",
                         d->key[j]+seclen+1,
                         d->val[j] ? d->val[j] : "");
             }
         }
+	fprintf(f, "\n");
     }
-    fprintf(f, "\n");
     return ;
 }
 
